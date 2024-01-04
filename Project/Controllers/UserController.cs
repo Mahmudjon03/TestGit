49ace01b6a53803080c774c08f8b5496104cddf3
mahmud <mahmudjonb515@gmail.com>
2024-01-05 00:14:18 +0500

    vbnm

---
+++ Project/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+
+
+namespace Project.Controllers
+{
+    public class UserController : Controller
+    {
+        List<User> users = new List<User>()
+        {
+         new User(){name="ali",email="asdfghjm", phone ="sdfghj"},
+         new User(){name="ali",email="asdfghjm", phone ="sdfghj"},
+         new User(){name="ali", email="asdfghjm", phone ="sdfghj"},
+        };
+        public int MyProperty { get; set; }
+
+
+        public IActionResult Index()
+        {
+
+            return View(users);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            // validate and insert
+            if (ModelState.IsValid)
+            {
+                users.Add(model);
+                Console.WriteLine(users.Count);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+
+    }
+}
---
+++ Project/Новая папка/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Project.Models;
+
+namespace Project.Новая_папка
+{
+    public class UserService
+    {
+        List<User> Usersers = new List<User>()
+             {
+            new User(){name="ali",email="asdfghjm", phone ="sdfghj"},
+              new User(){name="ali",email="asdfghjm", phone ="sdfghj"},
+                new User(){name="ali",email="asdfghjm", phone ="sdfghj"},
+        };
+
+        public void AddUser(User user)
+        {
+            
+            Usersers.Add(user);
+        }
+        public List<User> GetUsers()
+        {
+            return Usersers;
+        }
+
+
+    }
+}
